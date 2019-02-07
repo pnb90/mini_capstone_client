b122fd22c19b8d18ccd2e4c8084072bca0c90afd
pnb90 <phuocbui90@gmail.com>
2019-02-07 12:44:30 -0600

    added update path

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -5,6 +5,27 @@ class Client::ProductsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                      name: params[:name],
+                      price: params[:price],
+                      description: params[:description],
+                      supplier_id: params[:supplier_id]
+                    }
+
+    response = HTTP.post(
+                        "http://localhost:3000/api/products",
+                        form: client_params
+                        )
+
+    product = response.parse
+    redirect_to "/client/products/#{ product["id"] }"
+  end
+
   def show
     response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
     @product = response.parse
